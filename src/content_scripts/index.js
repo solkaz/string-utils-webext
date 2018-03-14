9325778f3b93aa5a6b7444dad4b0886f33139fe9
JMH <jheld135@gmail.com>
2018-03-13 21:25:47 -0500

    Copy text to Clipboard in content script

--- src/content_scripts/index.js
+++ src/content_scripts/index.js
@@ -1,3 +1,24 @@
+// https://stackoverflow.com/a/33928558
+const copyToClipboard = (text) => {
+    if (window.clipboardData && window.clipboardData.setData) {
+        return clipboardData.setData("Text", text); 
+    } else if (document.queryCommandSupported && document.queryCommandSupported("copy")) {
+        const textarea = document.createElement("textarea");
+        textarea.textContent = text;
+        textarea.style.position = "fixed";
+        document.body.appendChild(textarea);
+        textarea.select();
+        try {
+            return document.execCommand("copy");
+        } catch (ex) {
+            console.warn("Copy to clipboard failed.", ex);
+            return false;
+        } finally {
+            document.body.removeChild(textarea);
+        }
+    }
+}
+
 chrome.runtime.onMessage.addListener(({ textToSendToClipboard }) => {
-	// chrome.Clipboard.writeText(textToSendToClipboard);
+	copyToClipboard(textToSendToClipboard);
 })
